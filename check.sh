87259c12ef96c07e39f6330a8b23e5c1441c9c7b
mihai <mih.gogu@gmail.com>
2023-04-19 14:36:56 +0300

    Update report action to latest version

--- check.sh
+++ check.sh
@@ -13,6 +13,8 @@ if [ -z "$INPUT_PHP_IMAGE" ]; then
 fi
 
 echo -e "${BL}Info:${NC} Running PHP STAN with image: ${GR}$INPUT_PHP_IMAGE${NC}"
+echo -e "${BL}Info:${NC} STAN report path: ${GR}$INPUT_REPORT_PATH${NC}"
+echo -e "${BL}Info:${NC} Running command: ${GR}./vendor/bin/phpstan analyse app --no-interaction --no-ansi --error-format=checkstyle --memory-limit=512M > ${INPUT_REPORT_PATH} || true${NC}"
 docker run \
     --platform linux/amd64 \
     -v "$PWD":/var/www \
